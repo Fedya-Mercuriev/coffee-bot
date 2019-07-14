d8e8b1e79543babe90a8f8b8ca96b5744c9e9a8e
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-07-14 18:24:16 +0700

    Redone menu assembling function

--- src/util/keyboards.ts
+++ src/util/keyboards.ts
@@ -1,13 +1,31 @@
 import { Markup, ContextMessageUpdate, CallbackButton } from "telegraf";
 
-export function buildMenu<T>(ctx: ContextMessageUpdate, options: T[] ): any {
+export function buildMenu(options: any ): any {
     let result: Array<CallbackButton>[] = [];
 
-    options.forEach((item: any) => {
-        result.push([
-            Markup.callbackButton(item.title, JSON.stringify(item.data))
-        ])
-    });
+    for (let item in options) {
+        const data:any = {};
+        let title = '';
+
+        if (!options[item].data) {
+            for (let key in options[item]) {
+                if (key === 'title') {
+                    title = options[item][key];
+                } else {
+                    data[key] = options[item][key];
+                }
+            }
+
+            result.push([
+                Markup.callbackButton(title, JSON.stringify(data))
+            ]);
+        } else {
+            result.push([
+                Markup.callbackButton(options[item].title, JSON.stringify(options[item].data))
+            ]);
+
+        }
+    }
 
     return Markup.inlineKeyboard(result);
 }

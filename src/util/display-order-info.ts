0cf631a06925a8cad4ca58d8b3f50ea135cd8a4f
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-07-14 15:44:09 +0700

    Added a function that displays order info

---
+++ src/util/display-order-info.ts
@@ -0,0 +1,6 @@
+import { ContextMessageUpdate } from "telegraf";
+
+export default async function displayOrderInfo(ctx: ContextMessageUpdate): Promise<ReturnedMessage> {
+    return await ctx.replyWithHTML(ctx.session.orderInfoMsg,
+        {parse_mode: 'HTML', disable_notification: true});
+}

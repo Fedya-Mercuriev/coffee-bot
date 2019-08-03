b407269892b9c9a836c6ad0eae8649ee77764b4b
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-08-03 13:45:03 +0700

    Fixed message parsing

--- src/util/display-order-info.ts
+++ src/util/display-order-info.ts
@@ -1,6 +1,8 @@
 import { ContextMessageUpdate } from "telegraf";
 
 export default async function displayOrderInfo(ctx: ContextMessageUpdate): Promise<ReturnedMessage> {
-    return await ctx.replyWithHTML(ctx.session.orderInfoMsg,
-        {disable_notification: true});
+    return await ctx.replyWithHTML(
+        ctx.session.orderInfoMsg,
+        {parse_mode: 'HTML', disable_notification: true}
+        );
 }

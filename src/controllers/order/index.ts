b1751d2a9d3e9eae984061abbbd7d6697fb774ca
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-08-03 20:38:26 +0700

    Finished order scene

--- src/controllers/order/index.ts
+++ src/controllers/order/index.ts
@@ -1,14 +1,32 @@
 import _ from 'lodash';
 import { ContextMessageUpdate } from 'telegraf';
-import Stage from 'telegraf/stage';
 import Scene from 'telegraf/scenes/base';
+import Stage from 'telegraf/stage';
+import { buildOrderMenu } from "../../util/keyboards";
+import navigateScene from '../../middlewares/navigate-scene';
+import invokeFunction from '../../middlewares/invoke-function';
+import { updateOrderInfo } from './middlewares';
+import { init, finish, addNavigationToStructure } from './helpers';
+import displayOrderInfo from '../../util/display-order-info';
+import clearScene from '../../util/clear-scene';
+import dummy from './dummy.json';
 
+const sceneId = 'order';
 const { leave } = Stage;
-const menu = new Scene('menu');
+const order = new Scene(sceneId);
 
-menu.enter(async (ctx: ContextMessageUpdate) => {
-    await ctx.reply(ctx.i18n.t('scenes.order.welcome'));
-});
+order.use(
+    updateOrderInfo,
+    navigateScene,
+    invokeFunction
+);
+
+order.enter(async (ctx: ContextMessageUpdate) => {
+    // Process response and add links to scenes depending on whether a drink has different amounts or none
+    const menu = await addNavigationToStructure(dummy, ['order_amount', 'order_additions']);
+
+    // Removing messages from previous scene
+    await clearScene(ctx);
 
     if (!ctx.session.order) {
         init(ctx);
@@ -35,6 +53,4 @@ order.leave((ctx: ContextMessageUpdate) => {
    ctx.session.currentMenu.clear();
 });
 
-menu.command('back', leave());
-
-export default menu;
+export default order;

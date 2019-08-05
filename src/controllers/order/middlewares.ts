574cea25cc3fbcc9471f6157e785506c154ac489
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-08-05 23:46:16 +0700

    Fixes to middlewares

--- src/controllers/order/middlewares.ts
+++ src/controllers/order/middlewares.ts
@@ -4,15 +4,14 @@ import { updateOrderInfoMsg, composeOrderInfoMessage } from "./helpers";
 
 export async function updateOrderInfo(ctx: ContextMessageUpdate, next: Function) {
     if (ctx.updateType === 'callback_query') {
-        const args = JSON.parse(ctx.update.callback_query.data);
+        const args = ctx.session.currentMenu.get(ctx.update.callback_query.data);
 
         if (args.order) {
             await ctx.answerCbQuery('Обновляю ваш заказ...');
-            const orderData = await ctx.session.currentMenu.get(args.order);
 
             for (let prop in ctx.session.order) {
-                if (orderData.order.hasOwnProperty(prop)) {
-                    ctx.session.order[prop] = orderData.order[prop];
+                if (ctx.session.order.hasOwnProperty(prop) && args.order[prop]) {
+                    ctx.session.order[prop] = args.order[prop];
                 }
             }
             const orderInfo = await composeOrderInfoMessage(ctx);
--- src/middlewares/navigate-scene.ts
+++ src/middlewares/navigate-scene.ts
@@ -3,13 +3,12 @@ import { ContextMessageUpdate } from "telegraf";
 export default async function navigateToScene(ctx: ContextMessageUpdate, next: Function) {
     if (ctx.updateType === 'callback_query') {
         await ctx.answerCbQuery();
-        const args = JSON.parse(ctx.update.callback_query.data);
-        const sceneLink = (args.scene) ? args.scene : ctx.session.currentMenu.get(args.order).scene;
+        const args = ctx.session.currentMenu.get(ctx.update.callback_query.data);
 
-        if (sceneLink) {
+        if (args.scene) {
             ctx.scene.leave();
-            ctx.botScenes.iAmHere(ctx, sceneLink);
-            ctx.scene.enter(sceneLink);
+            ctx.botScenes.iAmHere(ctx, args.scene);
+            ctx.scene.enter(args.scene);
         } else {
             return next();
         }

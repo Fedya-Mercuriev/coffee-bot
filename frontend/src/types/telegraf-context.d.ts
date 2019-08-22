fb430ae6a54a7e8d799861e3d15c21e323ce4d10
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-08-23 00:23:04 +0700

    Fixed links

--- frontend/src/types/telegraf-context.d.ts
+++ frontend/src/types/telegraf-context.d.ts
@@ -1,10 +1,10 @@
-import { I18n } from 'src/types/telegraf-i18n';
+import { I18n } from 'telegraf-i18n';
 
 declare module 'telegraf' {
-    interface ContextMessageUpdate {
-        i18n: I18n;
-        scene: any;
-        botScenes: BotContext;
-        session: SessionStorage;
-    }
+  interface ContextMessageUpdate {
+    i18n: I18n;
+    scene: any;
+    botScenes: BotContext;
+    session: SessionStorage;
+  }
 }

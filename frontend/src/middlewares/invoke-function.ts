07547ccb7f636fd2a4c1f3b4954e3786cdfe853d
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-08-28 01:33:58 +0700

    Linted function invoker

--- frontend/src/middlewares/invoke-function.ts
+++ frontend/src/middlewares/invoke-function.ts
@@ -1,16 +1,19 @@
-import { ContextMessageUpdate } from "telegraf";
-import app from '../../_app';
+import { ContextMessageUpdate } from 'telegraf';
+import app from '../_app';
 
-export default async function invokeFunction(ctx: ContextMessageUpdate, next: Function): Promise<Function> {
-    if (ctx.updateType === 'callback_query') {
-        await ctx.answerCbQuery();
-        const args = ctx.session.currentMenu.get(ctx.update.callback_query.data);
+export default async function invokeFunction(
+  ctx: ContextMessageUpdate,
+  next: Function
+): Promise<Function> {
+  if (ctx.updateType === 'callback_query') {
+    await ctx.answerCbQuery();
+    const args = ctx.session.currentMenu.get(ctx.update.callback_query.data);
 
-        if (args.cb) {
-            app.call(args.cb.name, args.cb.args);
-        }
-        return next();
-    } else {
-        return next();
+    if (args.cb) {
+      app.call(args.cb.name, args.cb.args);
     }
+    return next();
+  } else {
+    return next();
+  }
 }

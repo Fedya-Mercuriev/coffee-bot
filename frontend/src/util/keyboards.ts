b4510bb82fdc48ef800f44bf18290372138ed878
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-08-28 00:04:31 +0700

    Fixes to keyboard builder

--- frontend/src/util/keyboards.ts
+++ frontend/src/util/keyboards.ts
@@ -1,60 +1,68 @@
-import { Markup, ContextMessageUpdate, CallbackButton } from "telegraf";
+import { Markup, ContextMessageUpdate, CallbackButton } from 'telegraf';
+import { EnumerableObject } from 'vendor';
 import generateRandomString from './generate-random-string';
-import _ from "lodash";
+import _ from 'lodash';
 
-/**
- * @param ctx - message update object
- * @param options - an object with
-* */
-export function buildMenu(ctx: ContextMessageUpdate, options: any): any {
-    let result: Array<CallbackButton>[] = [];
+function composeCallbackData(ctx: ContextMessageUpdate, menuItems: any): any {
+  let objectAccessor: string = generateRandomString(8);
+  let data: any = {};
 
-    for (let item in options) {
-        if (options.hasOwnProperty(item)) {
-            const title:string = options[item].title.replace(options[item].title.charAt(0), options[item].title.charAt(0).toUpperCase());
-            const data:any = composeCallbackData(ctx, options[item].data);
-
-            result.push([
-                Markup.callbackButton(title, data)
-            ]);
-        }
+  for (let key in menuItems) {
+    if (menuItems.hasOwnProperty(key)) {
+      if (key !== 'name') {
+        data[key] = menuItems[key];
+      }
     }
-
-    return Markup.inlineKeyboard(result);
-}
-
-export async function addBackButton(ctx: ContextMessageUpdate, menu: any): Promise<EnumerableObject> {
-    const previousScene = await ctx.botScenes.previousScene(ctx);
-
-    return Object.assign({}, menu, {
-        back: {
-            title: ctx.i18n.t('buttons.back'),
-            data: {
-                scene: previousScene
-            }
-        }
-    });
-}
-
-function composeCallbackData(ctx: ContextMessageUpdate, menuItems: any):any {
-    let objectAccessor:string = generateRandomString(8);
-    let data:any = {};
-
-    for (let key in menuItems) {
-        if (menuItems.hasOwnProperty(key)) {
-            if (key !== 'title') {
-                data[key] = menuItems[key];
-            }
-        }
-    }
-    /*
+  }
+  /*
      Object accessor is used to get an object with all necessary data
      Unfortunately telegram only allows 64-bit data, that's why have to
      store object in a separate property
     * */
-    while (ctx.session.currentMenu.has(objectAccessor)) {
-        objectAccessor = generateRandomString(8);
+  while (ctx.session.currentMenu.has(objectAccessor)) {
+    objectAccessor = generateRandomString(8);
+  }
+  ctx.session.currentMenu.set(objectAccessor, data);
+  return objectAccessor;
+}
+
+/**
+ * @param ctx - message update object
+ * @param options - an object with
+ * */
+export function buildMenu(
+  ctx: ContextMessageUpdate,
+  options: EnumerableObject
+): any {
+  let result: CallbackButton[][] = [];
+
+  for (let item in options) {
+    if (options.hasOwnProperty(item)) {
+      const name: string = options[item].name.replace(
+        options[item].name.charAt(0),
+        options[item].name.charAt(0).toUpperCase()
+      );
+      const data: any = composeCallbackData(ctx, options[item].data);
+
+      result.push([Markup.callbackButton(name, data)]);
     }
-    ctx.session.currentMenu.set(objectAccessor, data);
-    return objectAccessor;
+  }
+
+  return Markup.inlineKeyboard(result);
+}
+
+export async function addBackButton(
+  ctx: ContextMessageUpdate,
+  menu: any
+): Promise<EnumerableObject> {
+  const previousScene = await ctx.botScenes.previousScene(ctx);
+
+  return Object.assign({}, menu, {
+    back: {
+      name: ctx.i18n.t('buttons.back'),
+      data: {
+        scene: previousScene
+      }
+    }
+  });
 }

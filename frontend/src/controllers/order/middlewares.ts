a1906a85038b7d919dfa5e81cb434e6d8014bc21
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-08-31 16:53:53 +0700

    Linted code for order middlewares

--- frontend/src/controllers/order/middlewares.ts
+++ frontend/src/controllers/order/middlewares.ts
@@ -1,37 +1,43 @@
 import _ from 'lodash';
-import { ContextMessageUpdate } from "telegraf";
-import { updateOrderInfoMsg, composeOrderInfoMessage, finish } from "./helpers";
+import { ContextMessageUpdate } from 'telegraf';
+import { updateOrderInfoMsg, composeOrderInfoMessage, finish } from './helpers';
 
-export async function updateOrderInfo(ctx: ContextMessageUpdate, next: Function) {
-    if (ctx.updateType === 'callback_query') {
-        const args = ctx.session.currentMenu.get(ctx.update.callback_query.data);
+export async function updateOrderInfo(
+  ctx: ContextMessageUpdate,
+  next: Function
+) {
+  if (ctx.updateType === 'callback_query') {
+    const args = ctx.session.currentMenu.get(ctx.update.callback_query.data);
 
-        if (args.order) {
-            await ctx.answerCbQuery(`${ctx.i18n.t('status.update_order')}...`);
+    if (args.order) {
+      await ctx.answerCbQuery(`${ctx.i18n.t('status.update_order')}...`);
 
-            for (let prop in ctx.session.order) {
-                if (ctx.session.order.hasOwnProperty(prop) && args.order[prop]) {
-                    ctx.session.order[prop] = args.order[prop];
-                }
-            }
-            const orderInfo = await composeOrderInfoMessage(ctx);
-            await updateOrderInfoMsg(ctx, orderInfo);
+      for (let prop in ctx.session.order) {
+        if (ctx.session.order.hasOwnProperty(prop) && args.order[prop]) {
+          ctx.session.order[prop] = args.order[prop];
         }
-        return next();
+      }
+      const orderInfo = await composeOrderInfoMessage(ctx);
+      await updateOrderInfoMsg(ctx, orderInfo);
     }
     return next();
+  }
+  return next();
 }
 
-export async function returnToMainMenu(ctx: ContextMessageUpdate, next: Function) {
-    if (ctx.updateType === 'callback_query') {
-        const args = ctx.session.currentMenu.get(ctx.update.callback_query.data);
+export async function returnToMainMenu(
+  ctx: ContextMessageUpdate,
+  next: Function
+) {
+  if (ctx.updateType === 'callback_query') {
+    const args = ctx.session.currentMenu.get(ctx.update.callback_query.data);
 
-        if (args.scene === ctx.session.scenesMap[0]) {
-            await ctx.answerCbQuery(`${ctx.i18n.t('status.return_main_menu')}...`);
-            await finish(ctx);
-            return next();
-        } else {
-            return next();
-        }
+    if (args.scene === ctx.session.scenesMap[0]) {
+      await ctx.answerCbQuery(`${ctx.i18n.t('status.return_main_menu')}...`);
+      await finish(ctx);
+      return next();
+    } else {
+      return next();
     }
+  }
 }

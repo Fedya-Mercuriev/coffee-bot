0e48a22e3f9c966e173afd4c4bfdafa7d33134de
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-09-29 01:49:13 +0700

    Added locale extractor

---
+++ frontend/src/util/add-locale.ts
@@ -0,0 +1,15 @@
+import { ContextMessageUpdate } from 'telegraf';
+import MenuButton from 'menu-button';
+/**
+ * @param button - current button object;
+ * @param ctx - a context message update object;
+ * @param menuType - a page on which the menu is (main, order etc);
+ */
+
+export default function addLocaleToMenu(
+  button: MenuButton,
+  ctx: ContextMessageUpdate,
+  menuType: string
+): string {
+  return ctx.i18n.t(`menus.${menuType}.${button.name}`);
+}

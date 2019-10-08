6e70e7ff42181bdb3a5d118fcd80329b6f147635
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-10-09 01:00:46 +0700

    Fixed auth middleware

--- frontend/src/middlewares/auth.ts
+++ frontend/src/middlewares/auth.ts
@@ -5,6 +5,12 @@ export default function auth(
   ctx: ContextMessageUpdate,
   next: Function
 ): Function {
+  if (
+    ctx.updateType !== 'message' &&
+    ctx.updateSubTypes.indexOf('text') === -1
+  ) {
+    return next();
+  }
   const password = ctx.update.message.text;
 
   if (password.search('admin') !== -1) {
--- frontend/src/types/telegraf-context.d.ts
+++ frontend/src/types/telegraf-context.d.ts
@@ -4,6 +4,7 @@ import { BotContext, SessionStorage } from './vendor';
 declare module 'telegraf' {
   interface ContextMessageUpdate {
     i18n: I18n;
+    updateSubTypes: string[];
     scene: any;
     botScenes: BotContext;
     session: SessionStorage;

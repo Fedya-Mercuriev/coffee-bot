9541808746c80e18175e2d8e429abcb1325780b6
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-10-15 00:20:00 +0700

    Prettified scene cleaner

--- frontend/src/util/clear-scene.ts
+++ frontend/src/util/clear-scene.ts
@@ -1,15 +1,15 @@
-import { ContextMessageUpdate } from "telegraf";
+import { ContextMessageUpdate } from 'telegraf';
 
 /**
-* Removes messages collecting in scene
-* @param ctx - Message update object
-* */
+ * Removes messages collecting in scene
+ * @param ctx - Message update object
+ * */
 export default async function clearScene(ctx: ContextMessageUpdate) {
-    if (ctx.session.messages.hasMessages()) {
-        const messages = ctx.session.messages.storage;
-        for (let [key, message_id] of messages) {
-            await ctx.telegram.deleteMessage(ctx.chat.id, message_id);
-            await messages.delete(key);
-        }
+  if (ctx.session.messages.hasMessages()) {
+    const messages = ctx.session.messages.storage;
+    for (let [key, message_id] of messages) {
+      await ctx.telegram.deleteMessage(ctx.chat.id, message_id);
+      await messages.delete(key);
     }
+  }
 }

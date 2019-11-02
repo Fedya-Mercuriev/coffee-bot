094ab42864cf72256668ad192b9ba644a6eab2db
Fedya-Mercuriev <heyzhongguo@yahoo.com>
2019-11-02 19:10:37 +0700

    Fixed callback data composing

--- frontend/src/util/compose-order-data.ts
+++ frontend/src/util/compose-order-data.ts
@@ -1,12 +1,38 @@
-import { OrderItem } from 'order';
+import _ from 'lodash';
+import { GoodType } from 'good-type';
+import { Good } from 'good';
+import { GoodVolume } from 'good-volume';
 
-export default function composeOrderData(): void {
-  const result: { [key: string]: any } = {};
-  const menu = this.menu;
-  for (let key in menu) {
-    if (key !== 'description') {
-      result.order[key] = menu[key];
+/**
+ * Accepts a button and assigns an objects that contains all data necessary for order update on current step
+ * @param button - currently iterated button object
+ * @param options - an object of additional parameters (type - a type of item (good, volume, additive),
+ * stripKeys - an array (or string) of keys to include into result object)
+ */
+
+export default function composeOrderData<
+  T extends GoodType | Good | GoodVolume
+>(button: T, options: { type: string; stripKeys?: string | string[] }): T {
+  const orderObj: any = {};
+  let keysToExclude = ['name', 'description', 'url', 'scene', 'cb'];
+  const { type, stripKeys } = options;
+
+  orderObj.type = type;
+
+  if (stripKeys) {
+    if (!Array.isArray(stripKeys)) {
+      keysToExclude = _.without(keysToExclude, stripKeys);
+    } else {
+      keysToExclude = _.without(keysToExclude, ...stripKeys);
     }
   }
-  Object.assign(this.menu, result);
+
+  for (let key in button) {
+    if (button.hasOwnProperty(key)) {
+      if (keysToExclude.indexOf(key) === -1) {
+        orderObj[key] = button[key];
+      }
+    }
+  }
+  return Object.assign(button, { order: orderObj });
 }
